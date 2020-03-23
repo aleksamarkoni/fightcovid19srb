b83483f0e72f95d5c9ef7b1307b6607966035a02
markonni93 <marko.djurdjev93@gmail.com>
2020-03-23 20:57:32 +0100

    Add code to check necessarry location permissions

--- app/src/main/java/com/fightcorona/main/fragments/MapFragment.kt
+++ app/src/main/java/com/fightcorona/main/fragments/MapFragment.kt
@@ -1,9 +1,16 @@
 package com.fightcorona.main.fragments
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.fightcorona.di.Injectable
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -14,6 +21,10 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.fightcorona.R
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import timber.log.Timber
 
 class MapFragment : Fragment(), Injectable, OnMapReadyCallback {
@@ -31,9 +42,12 @@ class MapFragment : Fragment(), Injectable, OnMapReadyCallback {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        getLocationPermissions()
         mFusedLocationProviderClient =
             LocationServices.getFusedLocationProviderClient(requireActivity())
+    }
 
+    fun getMapAsync() {
         val map = childFragmentManager.findFragmentById(R.id.gmap_frag) as? SupportMapFragment
         map?.getMapAsync(this)
     }
@@ -43,23 +57,20 @@ class MapFragment : Fragment(), Injectable, OnMapReadyCallback {
             return
         }
         try {
-            if (checkPermission()) {
+            if (arePermissionsGranted()) {
                 mMap.isMyLocationEnabled = true
                 mMap.uiSettings.isMyLocationButtonEnabled = true
                 getDeviceLocation()
             } else {
                 mMap.isMyLocationEnabled = false
                 mMap.uiSettings.isMyLocationButtonEnabled = false
+                getLocationPermissions()
                 //mLastKnownLocation = null
                 //getLocationPermission()
             }
         } catch (e: SecurityException) {
             Timber.e("Exception: %s", e.message)
         }
-    }
-
-    private fun checkPermission(): Boolean {
-        return true
     }
 
     private fun getDeviceLocation() {
@@ -77,5 +88,62 @@ class MapFragment : Fragment(), Injectable, OnMapReadyCallback {
     override fun onMapReady(map: GoogleMap) {
         mMap = map
         updateLocationUi()
+    }
+
+    private fun getLocationPermissions() {
+        Dexter.withActivity(requireActivity())
+            .withPermissions(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            .withListener(object : MultiplePermissionsListener {
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    token?.continuePermissionRequest()
+                }
+
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                    when {
+                        report.areAllPermissionsGranted() -> {
+                            getMapAsync()
+                        }
+                        report.isAnyPermissionPermanentlyDenied -> showPermissionSettingsDialog()
+                        else -> showPermissionSettingsDialog()
+                    }
+                }
+            }).check()
+    }
+
+    private fun showPermissionSettingsDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle(getString(R.string.permission_needed))
+        builder.setMessage(getString(R.string.permission_dialog_desc))
+        builder.setCancelable(false)
+        builder.setPositiveButton(getString(R.string.go_to_settings)) { dialog, which ->
+            dialog.dismiss()
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            intent.data = Uri.parse("package:" + requireContext().packageName)
+            startActivity(intent)
+        }
+        builder.setNegativeButton(getString(R.string.cancel_button)) { dialog, which ->
+            dialog.dismiss()
+            activity?.finish()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun arePermissionsGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
     }
 }
