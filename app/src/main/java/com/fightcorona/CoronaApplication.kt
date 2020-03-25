a579653e9370a2bea2e72e3004d5c825d4101aed
markonni93 <marko.djurdjev93@gmail.com>
2020-03-25 18:17:53 +0100

    Implement sentry for error tracking

--- app/src/main/java/com/fightcorona/CoronaApplication.kt
+++ app/src/main/java/com/fightcorona/CoronaApplication.kt
@@ -1,12 +1,13 @@
 package com.fightcorona
 
-import android.app.Activity
 import android.app.Application
 import com.fightcorona.di.AppInjector
 import com.google.fightcorona.BuildConfig
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import io.sentry.Sentry
+import io.sentry.android.AndroidSentryClientFactory
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -18,6 +19,10 @@ class CoronaApplication : Application(), HasAndroidInjector {
     override fun onCreate() {
         super.onCreate()
         AppInjector.init(this)
+        Sentry.init(
+            "https://65d217755bd74bc9aa7181f6709e4fd4@sentry.io/5174833",
+            AndroidSentryClientFactory(this)
+        )
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
